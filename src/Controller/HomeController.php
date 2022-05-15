957100487b61c4147b02ff9f0f9f7c3ed641e413
Vladislav <v.verokha@keep-calm.ru>
2022-05-15 21:23:36 +0300

    add some routes

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,23 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(ProductRepository $productRepository): Response
     {
+        $products = $productRepository->findAll();
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'products' => $products
+        ]);
+    }
+
+    #[Route('/product/{id}', name: 'show_product')]
+    public function showProduct($id, ProductRepository $productRepository): Response
+    {
+        $product = $productRepository->find($id);
+
+        return $this->render('home/show_product.html.twig', [
+            'product' => $product
         ]);
     }
 }
