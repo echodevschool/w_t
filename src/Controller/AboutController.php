57d98d1c94142331bcd6cd1aea314ed78833c43f
litvinovaev <4863574@mail.ru>
2022-06-29 00:07:07 +0300

    aboutpage

--- src/Controller/AboutController.php
+++ src/Controller/AboutController.php
@@ -28,23 +28,34 @@ class AboutController extends AbstractController
             'about' => $aboutUs
         ]);
     }
+
+    public function aboutUs(Request $request, EntityManagerInterface $entityManager): \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+    {
+        $feedback = new Feedback();
+        $form = $this->createForm(AboutType::class, $feedback);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($feedback);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('about_page');
+        }
+
+
+        return $this->render('about/about.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
 }
-//    public function about(Request $request, EntityManagerInterface $entityManager)
-//    {
-//        $feedback = new Feedback();
-//        $form = $this->createForm(AboutType::class, $feedback);
-//        $form->handleRequest($request);
-//
-//        if ($form->isSubmitted() && $form->isValid()) {
-//            $entityManager->persist($feedback);
-//            $entityManager->flush();
-//
-//            return $this->redirectToRoute('about_page');
-//        }
-//
-//
-//        return $this->render('about/about.html.twig', [
-//            'form' => $form->createView()
-//        ]);
-//    }
-//}
+
+
+    function feedbackTelegram($message) {
+
+        $token = "5137970276:AAFUmw3IFBKcL4TvsMPY1d_4obI54s9oN_E";
+        $chat_id = "-396557422";
+
+        parser("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$message}");
+
+    }
+
